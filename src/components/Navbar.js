45ee8d899fca0f2952e766579e8a365205481a28
JassLamsir <jaswinderlamsir@gmail.com>
2023-07-18 21:34:59 +0530

    added images and boutton

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import pizza from "../assets/pizza.png";
+import Reorder2 from "../assets/reorder2.png";
 import { Link } from "react-router-dom";
+
 import "../styles/Navbar.css";
 
 function Navbar() {
+const [openLinks, setOpenLinks] = useState(false);
+
+const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+};
     return (
         <div className="navbar">
-            <div className="leftSide">
+            <div className="leftSide" id={openLinks ? "open" : "close"}>
                 <img src= {pizza} />
+                 <div className="hiddenLinks">
+                <Link to="/">Home</Link>
+                <Link to="/menu">Menu</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                </div> 
             </div>
             <div className="rightSide">
                 <Link to="/">Home</Link>
                 <Link to="/menu">Menu</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
+                <button onClick={toggleNavbar}>
+                    <img height='100' src={Reorder2}/>
+                </button>
             </div>
         </div>
     )
 }
 
 export default Navbar
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/pizza.png";
 
 function Home() {
-    return <div>hello world</div>
+    return (
+    <div className="home">
+        <div className="headerContainer" style={{ backgroundImage: `url(${BannerImage})`}}>
+            <h1>Jaswinder Singh</h1>
+            <p>PIZZA WEBSITE</p>
+        <Link to="/menu">
+            <button>ORDER NOW</button>
+        </Link>
+        </div>
+    </div>
+    );
 }
 
 export default Home;
