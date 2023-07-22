533d81babbd24ba7fb503b2db21e8c6e07431f0f
JassLamsir <jaswinderlamsir@gmail.com>
2023-07-22 17:00:11 +0530

    added footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,20 @@
 /* cfc */
-import React from "react";
+import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Footer() {
-    return <div></div>   
+    return (
+        <div className="footer">
+            <div className="socialMedia">
+                <InstagramIcon /> <FacebookIcon /> <TwitterIcon /> <LinkedInIcon />
+            </div>
+            <p> &copy: 2023 JaswinderWebsite</p>
+        </div>
+    )
 }
 
-export default Footer;
+export default Footer
+
